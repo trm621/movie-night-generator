be69ec428f74291f2ec6a60607a5ab5f8afd0ec6
Edelgard <jtboyman@gmail.com>
2021-11-03 20:26:18 -0400

    basic modal to ask if user would like to get food, go back, or continue to movie

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let activateModal = function() {
+    document.getElementById('movie-modal').classList.add("is-active");
+}
+
+let closeModal = function() {
+    document.getElementById('movie-modal').classList.remove("is-active");
+}
