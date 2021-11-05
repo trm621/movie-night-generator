bcd46dbea4f5a9572c7dcb4e9ea926cec20d19f2
Edelgard <jtboyman@gmail.com>
2021-11-05 12:22:31 -0400

    js dynamically creates list of genre options when start button is clicked, minor updates to modal functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@
 let mainPage = document.getElementById('main-page');
 
 //function to pull up movie confirm and ask if you want to add recipe
-let activateModal = function() {
+let activateMovieModal = function() {
     document.getElementById('movie-modal').classList.add("is-active");
 }
 //closes the 'moive-modal'
-let closeModal = function() {
+let closeMovieModal = function() {
     document.getElementById('movie-modal').classList.remove("is-active");
 }
 
@@ -25,93 +25,118 @@ let movieQuiz = function() {
     quizHeaderEl.textContent = "What kind of movie are we looking for?";
     quizContainerEl.appendChild(quizHeaderEl);
 
-    //create form
-    let quizFormEl = document.createElement("form");
-    quizContainerEl.appendChild(quizFormEl);
+    //create div for option 1
+    let quizOption1El = document.createElement("div");
+    quizContainerEl.appendChild(quizOption1El);
 
     //create form option 1 and label (comedy) then append
     let funnyInputEl = document.createElement("input");
     funnyInputEl.setAttribute("type","radio");
     funnyInputEl.setAttribute("id","comedy-input");
     funnyInputEl.setAttribute("value","comedy");
-    funnyInputEl.setAttribute("name","comedy-input");
+    funnyInputEl.setAttribute("name","movie-input");
 
     let funnyLabelEl = document.createElement("label");
     funnyLabelEl.setAttribute("for","comedy-input");
     funnyLabelEl.textContent = "  Something funny!";
 
-    quizFormEl.appendChild(funnyInputEl);
-    quizFormEl.appendChild(funnyLabelEl);
+    quizOption1El.appendChild(funnyInputEl);
+    quizOption1El.appendChild(funnyLabelEl);
+
+    //create div for option 2
+    let quizOption2El = document.createElement("div");
+    quizContainerEl.appendChild(quizOption2El);
 
     //create form option 2 and label (family) then append
     let familyInputEl = document.createElement("input");
     familyInputEl.setAttribute("type","radio");
     familyInputEl.setAttribute("id","family-input");
     familyInputEl.setAttribute("value","family");
-    familyInputEl.setAttribute("name","family-input");
+    familyInputEl.setAttribute("name","movie-input");
 
     let familyLabelEl = document.createElement("label");
     familyLabelEl.setAttribute("for","comedy-input");
     familyLabelEl.textContent = "  Something for the whole family!";
 
-    quizFormEl.appendChild(familyInputEl);
-    quizFormEl.appendChild(familyLabelEl);
+    quizOption2El.appendChild(familyInputEl);
+    quizOption2El.appendChild(familyLabelEl);
+
+    //create div for option 3
+    let quizOption3El = document.createElement("div");
+    quizContainerEl.appendChild(quizOption3El);
 
     //option 3(action)
     let actionInputEl = document.createElement("input");
     actionInputEl.setAttribute("type","radio");
     actionInputEl.setAttribute("id","action-input");
     actionInputEl.setAttribute("value","action");
-    actionInputEl.setAttribute("name","action-input");
+    actionInputEl.setAttribute("name","movie-input");
 
     let actionLabelEl = document.createElement("label");
     actionLabelEl.setAttribute("for","action-input");
     actionLabelEl.textContent = "  Something exciting!";
 
-    quizFormEl.appendChild(actionInputEl);
-    quizFormEl.appendChild(actionLabelEl);
+    quizOption3El.appendChild(actionInputEl);
+    quizOption3El.appendChild(actionLabelEl);
+
+    //create div for option 4
+    let quizOption4El = document.createElement("div");
+    quizContainerEl.appendChild(quizOption4El);
 
     //option 4(horror)
     let horrorInputEl = document.createElement("input");
     horrorInputEl.setAttribute("type","radio");
     horrorInputEl.setAttribute("id","scary-input");
     horrorInputEl.setAttribute("value","scary");
-    horrorInputEl.setAttribute("name","scary-input");
+    horrorInputEl.setAttribute("name","movie-input");
 
     let horrorLabelEl = document.createElement("label");
     horrorLabelEl.setAttribute("for","scary-input");
     horrorLabelEl.textContent = "  Something scary!";
 
-    quizFormEl.appendChild(horrorInputEl);
-    quizFormEl.appendChild(horrorLabelEl);
+    quizOption4El.appendChild(horrorInputEl);
+    quizOption4El.appendChild(horrorLabelEl);
+
+    //create div for option 5
+    let quizOption5El = document.createElement("div");
+    quizContainerEl.appendChild(quizOption5El);
 
     //option 5(romance)
     let romanceInputEl = document.createElement("input");
     romanceInputEl.setAttribute("type","radio");
     romanceInputEl.setAttribute("id","romance-input");
     romanceInputEl.setAttribute("value","romance");
-    romanceInputEl.setAttribute("name","romance-input");
+    romanceInputEl.setAttribute("name","movie-input");
 
     let romanceLabelEl = document.createElement("label");
     romanceLabelEl.setAttribute("for","romance-input");
     romanceLabelEl.textContent = "  Something romantic!";
 
-    quizFormEl.appendChild(romanceInputEl);
-    quizFormEl.appendChild(romanceLabelEl);
+    quizOption5El.appendChild(romanceInputEl);
+    quizOption5El.appendChild(romanceLabelEl);
+
+    //create div for option 6
+    let quizOption6El = document.createElement("div");
+    quizContainerEl.appendChild(quizOption6El);
 
     //option 6(dramatic)
     let dramaInputEl = document.createElement("input");
     dramaInputEl.setAttribute("type","radio");
     dramaInputEl.setAttribute("id","drama-input");
     dramaInputEl.setAttribute("value","drama");
-    dramaInputEl.setAttribute("name","drama-input");
+    dramaInputEl.setAttribute("name","movie-input");
 
     let dramaLabelEl = document.createElement("label");
     dramaLabelEl.setAttribute("for","drama-input");
     dramaLabelEl.textContent = "  Something dramatic!";
 
-    quizFormEl.appendChild(dramaInputEl);
-    quizFormEl.appendChild(dramaLabelEl);
+    quizOption6El.appendChild(dramaInputEl);
+    quizOption6El.appendChild(dramaLabelEl);
+
+    //finished selecting button
+    let doneMovieButttonEl = document.createElement("button");
+    doneMovieButttonEl.textContent = "All Set!";
+    quizContainerEl.appendChild(doneMovieButttonEl);
 };
 
 //event listener to create quiz form when "get started!" is pressed
