0f2a42597d093f9fedee8d4ea3aa8f01813316bc
Edelgard <jtboyman@gmail.com>
2021-11-05 10:40:07 -0400

    put title text into a div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
+//function to pull up movie confirm and ask if you want to add recipe
 let activateModal = function() {
     document.getElementById('movie-modal').classList.add("is-active");
 }
-
+//closes the 'moive-modal'
 let closeModal = function() {
     document.getElementById('movie-modal').classList.remove("is-active");
 }
 
+//generates the html elements to ask about what type of movie you want when you click start
+let movieQuiz = function() {
+
+}
